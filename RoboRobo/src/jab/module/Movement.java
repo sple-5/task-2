f92e40f01c19ec13c19cf5289d6a2aabbc4e8ede
yudiptr <yudiputrasabri789@gmail.com>
2025-03-13 22:52:31 +0700

    feat: keyboard movement

--- RoboRobo/src/jab/module/Movement.java
+++ RoboRobo/src/jab/module/Movement.java
@@ -1,5 +1,7 @@
 package jab.module;
 
+import java.awt.event.InputEvent;
+
 import robocode.Event;
 
 /**
@@ -16,14 +18,59 @@ public class Movement extends Part {
 	}
 
 	public void move() {
-		bot.setMaxVelocity(8);
-		bot.setAhead(10000);
+		bot.setAhead(Double.POSITIVE_INFINITY * moveDirection);
+		bot.setTurnRight(45 * turnDirection);
+		bot.execute();
 	}
 
 	public void listen(Event e) {
-		if (e instanceof robocode.HitWallEvent) {
-			bot.setTurnRight(45);
+	}
+
+	public void listenInput(InputEvent e) {
+		if (e instanceof java.awt.event.KeyEvent) {
+			int keyID = ((java.awt.event.KeyEvent) e).getID();
+			int keyCode = ((java.awt.event.KeyEvent) e).getKeyCode();
+
+			if (keyID == 401) { // 401 = KeyEvent.KEY_PRESSED
+				switch (keyCode) {
+				case 38: // VK_UP
+					moveDirection = 1;
+					isMoving = true;
+					break;
+				case 40: // VK_DOWN
+					moveDirection = -1;
+					isMoving = true;
+					break;
+				case 39: // VK_RIGHT
+					turnDirection = 1;
+					break;
+				case 37: // VK_LEFT
+					turnDirection = -1;
+					break;
+				}
+				move(); // Pastikan move() dieksekusi setiap kali tombol ditekan
+			}
+
+			else if (keyID == 402) { // 402 = KeyEvent.KEY_RELEASED
+				switch (keyCode) {
+				case 38: // VK_UP
+				case 40: // VK_DOWN
+					moveDirection = 0;
+					isMoving = false;
+					bot.setAhead(0); // Stop gerakan saat tombol dilepas
+					break;
+				case 39: // VK_RIGHT
+				case 37: // VK_LEFT
+					turnDirection = 0;
+					break;
+				}
+				move(); // Panggil move() agar posisi update setelah tombol dilepas
+			}
 		}
 	}
+
+	int moveDirection = 0;
+	int turnDirection = 0;
+	boolean isMoving = false;
 
 }
