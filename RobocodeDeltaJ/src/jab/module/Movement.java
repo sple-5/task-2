602cc661eb5381e2ff720039d09c9ac33f151962
Edward-Salim <edwardsalim29@gmail.com>
2025-03-14 14:58:08 +0700

    feat: Targeting Delta

--- RobocodeDeltaJ/src/jab/module/Movement.java
+++ RobocodeDeltaJ/src/jab/module/Movement.java
@@ -1,5 +1,7 @@
 package jab.module;
 
+import java.awt.event.InputEvent;
+
 import robocode.Event;
 
 /**
@@ -24,6 +26,10 @@ public  class  Movement  extends Part {
 	}
 	
 	public void listen(Event e) {
+	}
+
+	
+	public void listenInput(InputEvent e) {
 	}
 
 
